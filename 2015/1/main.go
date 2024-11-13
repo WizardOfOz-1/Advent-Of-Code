c5bf00ed752d59d8f315dd67ae8e0fcffd5bbef0
Wizard <wizard.of.oz.n01@gmail.com>
2024-11-13 18:20:46 +0545

    Completed day 1

--- 2015/1/main.go
+++ 2015/1/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func readFile() int {
-	story := 0
+	story, pos := 0, 1
 	file, err := os.Open("input")
 	if err != nil {
 		panic("Couldnt open file")
@@ -22,11 +22,17 @@ func readFile() int {
 		} else if char == ")" {
 			story--
 		}
+
+		if story < 0 {
+			print("Reached -1 at ", pos)
+			break
+		}
+        pos++
 	}
 	return story
 }
 func main() {
-    res := readFile()
-    print(res)
+	res := readFile()
+	print(res)
 
 }
